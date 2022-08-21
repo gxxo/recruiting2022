dcfbd008d232bfb325230f46706ae5e7649761c0
gaga <jcky0511@gmail.com>
2022-08-21 22:45:06 +0900

    20220821

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+          <Routes>
+            <Route exact path='/' element = {<LandingPage />} />
+            <Route path='/LoginPage' element = {<LoginPage />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import "./LoginPage.css";
+import styled from "styled-components";  //React 컴포넌트 스타일링
+
+function LoginPage() {
+
+    const Input = styled.input`
+        position: relative;
+        width: 350px;
+        height: 35px;
+        outline-style: none;
+        background: #D9D9D9;
+        border: none;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 20px;
+        padding-left: 15px;
+        margin-left: 25px;
+        font-size: 20px;
+    `;
+
+    const Button = styled.button`
+        width: 600px;
+        height: 60px;
+        background: #ec1468;
+        border-radius: 30px;
+        color: white;
+        font-size: 24px;
+        font-weight: 600;
+        text-align: center;
+        margin-top: 60px;
+        margin-bottom: 20px;
+        border: none;
+        box-shadow: 0px 10px 20px rgba(236, 20, 104, 0.35);
+        
+    `;
+
+
+
+    return (
+        <div class="back">
+            <div class="nav">
+                <div class="logo">
+                    이화이언
+                </div>
+
+
+                <div class="nav_but">
+                    <a href="">지원하기</a>
+                    <a href="">결과 확인</a>
+                    <a href="">과제 제출</a>
+                </div>
+            </div>
+
+            <img src="client\src\배너.png" alt=""></img>
+
+
+            <div class="Box">
+                <div>
+                    <h1>지원하기</h1>
+                </div>
+
+                <form>
+                    <table class='table'>
+                        <tr class='info'>
+                            <td class='info_title'>성명</td>
+                            <td class='info_input'><Input /></td>
+                        </tr>
+                        <tr class='info'>
+                            <td class='info_title'>학번 7자리</td>
+                            <td class='info_input'><Input /></td>
+                        </tr>
+                        <tr class='info'>
+                            <td class='info_title'>이화이언 ID</td>
+                            <td class='info_input'><Input /></td>
+                        </tr>
+                    </table>
+                </form>
+
+                <Button type="submit">제출하기</Button>
+                <p>
+                    <a href="" class="download">지원서 다운로드</a>
+                    <a href="" class="download_upload">지원서 제출</a>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage
